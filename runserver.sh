41ebb49bc1bc309805a96dd67374d45cf4290301
Patryk <patrykprause@gmail.com>
2023-06-09 17:16:55 +0200

    spelling mistake on celery.sh

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,3 @@
 python manage.py migrate
 python manage.py createsuperuser --no-input
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'patrykprause@gmail.com', 'admin')" | python manage.py shell
 gunicorn miejski.wsgi --bind=0.0.0.0:80
